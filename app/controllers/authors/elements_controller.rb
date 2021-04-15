ddd56a49438d384c6e842867cdc34042ef59182a
Shondey.Nguyen <Shondey.Nguyen@southeasttech.edu>
2021-04-14 19:39:23 -0500

    4/14 first push

---
+++ app/controllers/authors/elements_controller.rb
@@ -0,0 +1,48 @@
+module Authors
+    class ElementsController < AuthorsController
+      before_action :set_post
+      before_action :set_element, only: [:update, :destroy]
+
+      # POST /elements
+      def create
+        @element = @post.elements.build
+
+        if @element.save
+          redirect_to @post
+        else
+          redirect_to @post, notice: @element.errors.full_messages.join(". ") << "."
+        end
+      end
+
+      # PATCH/PUT /elements/1
+      def update
+        if @element.update(element_params)
+          redirect_to @element, notice: 'Element was successfully updated.'
+        else
+          render :edit
+        end
+      end
+
+      # DELETE /elements/1
+      def destroy
+        @element.destroy
+        redirect_to elements_url, notice: 'Element was successfully destroyed.'
+      end
+
+      private
+      def set_post
+        @post = current_author.posts.find(params[:post_id])
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_element
+          @element = @post.elements.find(params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def element_params
+          params.require(:element).permit(:element_type, :content)
+        end
+    end
+end
